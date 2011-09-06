9337d41429d1f81490b1d9754981546ef1abb46f
nrkkalyan <nrkkalyan@gmail.com>
2011-09-06 23:21:38 +0200

    Changes to search by HotelName

--- src/com/unitedcaterers/server/RMIUCServerImpl.java
+++ src/com/unitedcaterers/server/RMIUCServerImpl.java
@@ -1,50 +1,50 @@
 package com.unitedcaterers.server;
 
-import com.unitedcaterers.UCServer;
-import com.unitedcaterers.util.UCException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-
-
+import com.unitedcaterers.UCServer;
+import com.unitedcaterers.util.UCException;
 
 /**
- * Since we need to make this a RMI Remote object we have to extend it from UnicastRemoteObject and
- * so we cannot extend it from UCServer. Instead, we will instantiate UCServer object and delegate all the calls to it.
- * In affect, it is exactly same as UCServer except that it is a remote class.
+ * Since we need to make this a RMI Remote object we have to extend it from
+ * UnicastRemoteObject and so we cannot extend it from UCServer. Instead, we
+ * will instantiate UCServer object and delegate all the calls to it. In affect,
+ * it is exactly same as UCServer except that it is a remote class.
  */
 
 public class RMIUCServerImpl extends UnicastRemoteObject implements UCServer {
-    
-    private UCServerImpl ucs;
-    
-    public RMIUCServerImpl(String dbfilename, String magiccode) throws RemoteException, UCException {
-        ucs = new UCServerImpl(dbfilename, magiccode);
-    }
-    
-    @Override
-    public String[][] searchCaterersByMaxGuests(int maxGuests) throws RemoteException, UCException {
-        return ucs.searchCaterersByMaxGuests(maxGuests);
-    }
-    
-    @Override
-    public String[][] searchCaterersByLocation(String location) throws RemoteException, UCException {
-        return ucs.searchCaterersByLocation(location);
-    }
-    
-    @Override
-    public String[][] searchCaterersByMaxGuestsAndLocation(int maxGuests, String location) throws RemoteException, UCException {
-        return ucs.searchCaterersByMaxGuestsAndLocation(maxGuests, location);
-    }
-    
-    @Override
-    public String[][] getAllCaterers() throws RemoteException, UCException {
-        return ucs.getAllCaterers();
-    }
-    
-    @Override
-    public boolean bookCaterer(String customerid, String[] originalData) throws RemoteException, UCException {
-        return ucs.bookCaterer(customerid, originalData);
-    }
-    
+	
+	private final UCServerImpl	ucs;
+	
+	public RMIUCServerImpl(String dbfilename, String magiccode) throws RemoteException, UCException {
+		ucs = new UCServerImpl(dbfilename, magiccode);
+	}
+	
+	@Override
+	public String[][] searchCaterersByHotelName(String hotelName) throws RemoteException, UCException {
+		return ucs.searchCaterersByHotelName(hotelName);
+	}
+	
+	@Override
+	public String[][] searchCaterersByLocation(String location) throws RemoteException, UCException {
+		return ucs.searchCaterersByLocation(location);
+	}
+	
+	@Override
+	public String[][] searchCaterersByHotelNameAndLocation(String hotelName, String location) throws RemoteException,
+			UCException {
+		return ucs.searchCaterersByHotelNameAndLocation(hotelName, location);
+	}
+	
+	@Override
+	public String[][] getAllCaterers() throws RemoteException, UCException {
+		return ucs.getAllCaterers();
+	}
+	
+	@Override
+	public boolean bookCaterer(String customerid, String[] originalData) throws RemoteException, UCException {
+		return ucs.bookCaterer(customerid, originalData);
+	}
+	
 }
